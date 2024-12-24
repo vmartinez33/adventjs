ee23aebc53aa2e1b91787100ccda5c82dc05dfe1
Víctor Martínez <108585032+vmartinez33@users.noreply.github.com>
2024-12-24 13:26:36 +0100

    Challenge 2 made with Python

---
+++ 2024/Python/Challenge#2.py
@@ -0,0 +1,47 @@
+from typing import List
+
+def create_frame(names: List[str]) -> str:
+    largest_name = ""
+    for name in names:
+        if len(name) > len(largest_name):
+            largest_name = name
+    largest_length = len(largest_name)
+    
+    frame = '*' * (largest_length + 4) + "\n"
+    for name in names:
+        frame += "* " + name.ljust(largest_length) + " *\n"
+    frame += '*' * (largest_length + 4)
+    
+    return frame
+
+
+frame1 = create_frame(['midu', 'madeval', 'educalvolpz'])
+print(frame1)
+# Expected result:
+'''
+***************
+* midu        *
+* madeval     *
+* educalvolpz *
+***************
+'''
+
+frame2 = create_frame(['midu'])
+print(frame2)
+# Expected result:
+'''
+********
+* midu *
+********
+'''
+
+frame3 = create_frame(['a', 'bb', 'ccc'])
+print(frame3)
+# Expected result:
+'''
+*******
+* a   *
+* bb  *
+* ccc *
+*******
+'''
