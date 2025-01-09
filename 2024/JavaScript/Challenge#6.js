920dee7a65f77e5cec5a2b5c3f7dd42e1370b9ca
Víctor Martínez <108585032+vmartinez33@users.noreply.github.com>
2025-01-09 18:55:00 +0100

    Challenge 6 made with JavaScript

---
+++ 2024/JavaScript/Challenge#6.js
@@ -0,0 +1,44 @@
+/** @param {string[]} box
+ *  @returns {boolean} True if the gift is inside the box
+ */
+function inBox(box) {
+    for (let i = 1; i < box.length - 1; i++) {
+        if (box[i].substring(1, box[i].length - 1).includes('*')) {
+            return true;
+        }
+    }
+    return false
+}
+
+
+const bool = inBox([
+    "###",
+    "#*#",
+    "###"
+]) // ➞ true
+console.log(bool);
+
+const bool2 = inBox([
+    "####",
+    "#* #",
+    "#  #",
+    "####"
+]) // ➞ true
+console.log(bool2);
+
+const bool3 = inBox([
+    "#####",
+    "#   #",
+    "#  #*",
+    "#####"
+]) // ➞ false
+console.log(bool3);
+
+const bool4 = inBox([
+    "#####",
+    "#   #",
+    "#   #",
+    "#   #",
+    "#####"
+]) // ➞ false
+console.log(bool4);
